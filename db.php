14f393d787d7788c9b40f2ba1a44fa55b2609838
VRVG1 <viravago@gmail.com>
2023-08-09 20:38:23 -0600

    Subiendo el primer avance
    Se creo la coneccion con la base de datos postgresql
    Se creo una funcion para crear tokens jwt
    Se creo una funcion para validar los jwt

---
+++ db.php
@@ -0,0 +1,51 @@
+<?php
+
+class DBconection
+{
+   private $dbname;
+   private $user;
+   private $password;
+   private $pdo;
+
+   public function __construct($dbname, $user, $password)
+   {
+      $this->dbname = $dbname;
+      $this->user = $user;
+      $this->password = $password;
+   }
+   public function conectar()
+   {
+      try {
+         # Se crea un objeto PDO (PHP Data Objects) para manejar la conexion con la base de datos.
+         $this->pdo = new PDO("pgsql:dbname=$this->dbname", $this->user, $this->password);
+         # Se establecen los atributos ATTR_ERRMODE para controlar como se manejaran los errores que puedan ocurrir durante la conexion
+         # Y el atributo ERRMODE_EXCEPTION indica que los errores se manejaran lanzando excepciones
+         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $exception) {
+         echo "ERROR: " . $exception->getMessage();
+      }
+   }
+
+   public function desconectar()
+   {
+      $this->pdo = null;
+      echo "Base de datos desconectada";
+   }
+
+   public function ejecutarConsultaArbitraria($consulta)
+   {
+      $resultado = $this->pdo->query($consulta);
+      return $resultado->fetchAll(PDO::FETCH_ASSOC);
+   }
+
+   public function getUsuarios()
+   {
+      # Se realiza una consulta general a la tabla usuarios
+      $resultado = $this->pdo->query("SELECT * FROM usuarios;");
+      # Se almacena el arreglo resultante en una variable
+      $filas = $resultado->fetchAll(PDO::FETCH_ASSOC);
+      # Se tranforma el arreglo a json y se retorna
+      return json_encode($filas);
+   }
+}
+?>
---
+++ jwt_utils.php
@@ -0,0 +1,84 @@
+<?php
+// recibe la informacion a docodificar intercambiando + / remplazan a - _
+// respectivamente y se eliminan los caracteres =
+function base64url_encode($data)
+{
+    return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+}
+// Genera un jwt con los datos que recibe de la peticion
+function generate_jwt($headers, $payload, $secret = 'secret')
+{
+    // Codificar la cabecera en base64 usando el metodo creado
+    $headers_encoded = base64url_encode(json_encode($headers));
+    // Codificar el payload en base64 usando el metodo creado
+    $payload_encoded = base64url_encode(json_encode($payload));
+    // Crea una firma HMAC-SHA256
+    $signature = hash_hmac('SHA256', "$headers_encoded.$payload_encoded", $secret, true);
+    // Codificar la firma creada en base64url
+    $signature_encoded = base64url_encode($signature);
+    // Combinr todos los componentes en un JWT
+    $jwt = "$headers_encoded.$payload_encoded.$signature_encoded";
+    // regresar el JWT
+    return $jwt;
+}
+
+function validate_jwt($jwt, $secret = 'secret')
+{
+
+    // split the jwt
+    $tokenParts = explode('.', $jwt);
+    // Decodificar la primara parte del token
+    $header = base64_decode($tokenParts[0]);
+    // Decodificar la segunda parte del token
+    $payload = base64_decode($tokenParts[1]);
+    // Decodificar la tercera parte del token
+    $signature = $tokenParts[2];
+
+    // Verificar si el tiempo del token a expirado
+    $expiration = json_decode($payload)->exp;
+    $is_token_expired = ($expiration - time()) > 0;
+    // Creando una firma basada en la cabecera y el payload usando la llave (secret)
+    // Codificando cabecera
+    $header_encode = base64url_encode($header);
+    // Codificando payload
+    $payload_encode = base64url_encode($payload);
+    // Creando la firma
+    $signature_build = hash_hmac('SHA256', "$header_encode.$payload_encode", $secret, true);
+    // Codificando la firma
+    $signature_encode = base64url_encode($signature_build);
+    // verificar si la firma recibida en igual a la firma creada
+    $is_signature_valid = ($signature_encode === $signature);
+    // Retornamos verdadero o falso dependiendo si el token a expirado y la firma es valida
+    return ($is_token_expired && $is_signature_valid);
+}
+
+function get_auth_header()
+{
+    $header = null;
+
+    if (isset($_SERVER['Authorization'])) {
+        $header = trim($_SERVER['Authorization']);
+    } else if (isset($_SERVER['HTTP_Authorization'])) {
+        $header = trim($_SERVER['HTTP_Authorization']);
+    } else if (function_exists('apache_request_headers')) {
+        $requestHeader = apache_request_headers();
+        $requestHeader = array_combine(array_map('ucwords', array_keys($requestHeader)), array_values($requestHeader));
+        if (isset($requestHeader['Authorization'])) {
+            $header = trim($requestHeader['Authorization']);
+        }
+    }
+    return $header;
+
+}
+
+function get_bearer_token()
+{
+    $header = get_auth_header();
+
+    // Obtener el token de la cabecera
+    if (!empty($header) && preg_match('/Bearer\s(\S+)/', $header, $matches)) {
+        return $matches[1];
+    }
+    return null;
+}
+?>
